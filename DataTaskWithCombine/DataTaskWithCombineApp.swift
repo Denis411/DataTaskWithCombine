96c898abfabcf9db864ea8803ccb7b59f0913c1a
Denis <theveryengineer@gmail.com>
2023-05-15 20:17:58 +0500

    Initial Commit

---
+++ DataTaskWithCombine/DataTaskWithCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataTaskWithCombineApp.swift
+//  DataTaskWithCombine
+//
+//  Created by Apex on 15/5/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct DataTaskWithCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
